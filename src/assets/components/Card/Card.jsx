683cebfdd0bf9c4e27622a86a15b966722daa7ac
sariunar <rinchinovasariuna@gmail.com>
2023-05-12 21:53:57 +0800

    change

--- src/assets/components/Card/Card.jsx
+++ src/assets/components/Card/Card.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState, useRef, useEffect } from 'react';
+//import { useState, useRef, useEffect } from 'react';
 import './Card.css';
 
 class Card extends React.Component {
